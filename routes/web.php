1461be40b56d5bb7479080febbfc25ff9b1a5511
Zaryab <109843326+mzaryabuddin1@users.noreply.github.com>
2024-12-13 21:21:27 +0500

    added

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     // PASS DATA WITH ROUTE
+//     $data = ['title' => 'Example Project', 'company_name' => 'Example Company', 'fav_icon' => 'https://cdn-icons-png.flaticon.com/512/5220/5220478.png', 'short_name' => 'EX', 'coprights' => '2024'];
+//     return view('home', $data);
+// });
+
+// Route::get('/empty', function () {
+//     return view('empty');
+// });
+
+// Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
+
+
 Route::get('/', function () {
     // PASS DATA WITH ROUTE
     $data = ['title' => 'Example Project', 'company_name' => 'Example Company', 'fav_icon' => 'https://cdn-icons-png.flaticon.com/512/5220/5220478.png', 'short_name' => 'EX', 'coprights' => '2024'];
     return view('home', $data);
 });
-
-Route::get('/empty', function () {
-    return view('empty');
-});
-
-Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
